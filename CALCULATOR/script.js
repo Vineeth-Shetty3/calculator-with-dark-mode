3a1177b033b6bf3a9f8478235342562b555bc795
Vineeth Shetty <shettyvineeth1999@gmail.com>
2022-12-25 17:29:30 +0530

    Add files via upload

---
+++ CALCULATOR/script.js
@@ -0,0 +1,24 @@
+let buttons =document.querySelector('.buttons');
+let btn=document.querySelector('span');
+let value=document.querySelector('value');
+let togglebtn= document.querySelector('.togglebtn')
+let body=document.querySelector('body')
+
+for( let i=0; i<btn.length;i++){
+    btn[i].addEventListener("click" , function(){
+
+        if(this.innerHTML=="="){
+            value.innerHTML=eval(value.innerHTML);
+        }else{
+            if(this.innerHTML=="Clear"){
+                value.innerHTML="  ";
+            }
+            else{
+                value.innerHTML ==this.innerHTML;
+            }
+        }
+    })
+}
+togglebtn.onclick = function(){
+    body.classList.toggle("dark");
+}
